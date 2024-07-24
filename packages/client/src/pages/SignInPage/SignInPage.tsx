31271640f84b2b2b8a0a804cace83a047bc186b6
Anton Zabelin <azabelin@t1-consulting.ru>
2024-07-24 11:56:12 +0300

    Base and auth API, Sign up, sign in and logout user propcess. Helper

--- packages/client/src/pages/SignInPage/SignInPage.tsx
+++ packages/client/src/pages/SignInPage/SignInPage.tsx
@@ -1,3 +1,62 @@
+import { useState } from 'react';
+import { TSignInRequest, TUser } from '../../shared/types/user';
+import AuthAPI from '../../services/api/auth-api';
+import { Link } from 'react-router-dom';
+
+const authAPI = new AuthAPI();
+
 export const SignInPage = () => {
-  return <div>SignInPage</div>;
+  const [user, setUser] = useState<TUser>({} as TUser);
+  const [data, setData] = useState<TSignInRequest>({} as TSignInRequest);
+
+  const login = 'l586';
+  const password = '123!!321';
+
+  const auth = () => {
+    const _data: TSignInRequest = {
+      login,
+      password,
+    };
+
+    setData(_data);
+
+    authAPI.signin(_data, isOk, errorHandler);
+  };
+
+  const isOk = () => {
+    authAPI.getuser(updUserData, errorHandler);
+  };
+
+  const updUserData = (u: TUser) => {
+    setUser(u);
+  };
+
+  const errorHandler = (err: unknown) => {
+    console.error('EEEEEEEEEEEE', err);
+  };
+
+  return (
+    <>
+      <div>SignInPage</div>
+      <hr />
+      <div>Login: {login}</div>
+      <div>Password: {password}</div>
+      <button onClick={auth}>Sign in</button>
+
+      <div>
+        <Link to="/signup">Register</Link>
+      </div>
+
+      <hr />
+      <div>
+        <strong>Send login data</strong>
+      </div>
+      <pre>{JSON.stringify(data, null, '    ')}</pre>
+      <hr />
+      <div>
+        <strong>Login user data</strong>
+      </div>
+      <pre>{JSON.stringify(user, null, '    ')}</pre>
+    </>
+  );
 };
