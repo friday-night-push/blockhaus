c73875cf96cfc086ff610df13505413b260359b7
Khizri Makhmudov <kh.makhmudov@me.com>
2024-07-28 18:40:31 +0300

    refactor(FNP-45): update import paths and configurations for consistency and maintainability
    - Updated import paths to use absolute paths in SignUpPage, SignInPage, ErrorBoundaryPage, and auth-api for consistency
    - Renamed `error-doundary.tsx` to `error-boundary.tsx` for correct spelling
    - Added alias for `src` directory in Vite configuration for cleaner import paths
    - Modified tsconfig.json to include `baseUrl` and `paths` for using `src` alias
    - Rearranged and updated compiler options in tsconfig.json for better configuration management

--- packages/client/src/services/error/error-boundary.tsx
+++ packages/client/src/services/error/error-boundary.tsx
@@ -1,5 +1,5 @@
 import React, { Component, ErrorInfo, ReactNode } from 'react';
-import Helpers from '../../utils/helpers';
+import Helpers from 'src/utils/helpers';
 
 interface Props {
   children?: ReactNode;
