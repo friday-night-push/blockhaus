fbbebb8bb3fdeba135b2d6fd2d45a1353d3fede3
Ekaterina Kuzina <katya.cuzina2011@yandex.ru>
2024-08-08 06:02:19 +0300

    FNP-58: add store by redux Toolkit

---
+++ packages/client/src/store/slice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface ExampleState {
+  name: string;
+}
+
+const initialState: ExampleState = {
+  name: '',
+};
+
+const exampleSlice = createSlice({
+  name: 'example',
+  initialState,
+  reducers: {
+    setExample: (state, action: PayloadAction<string>) => {
+      state.name = action.payload;
+    },
+    clearExample: state => {
+      state.name = '';
+    },
+  },
+});
+
+export const { setExample, clearExample } = exampleSlice.actions;
+
+export default exampleSlice.reducer;
---
+++ packages/client/src/store/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
+import { api } from './api';
+import exampleReducer from './slice';
+
+export const store = configureStore({
+  reducer: {
+    example: exampleReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
