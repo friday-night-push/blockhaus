861b5fa71848d6df209eab22cd02178063c6efc5
Khizri Makhmudov <kh.makhmudov@me.com>
2024-08-10 19:58:59 +0300

    refactor(FNP-55): remove CSS modules from Form and Logo components

--- packages/client/src/components/molecules/Form/Form.tsx
+++ packages/client/src/components/molecules/Form/Form.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-
 import { Text } from '@gravity-ui/uikit';
+
 import type { FormikErrors, FormikHelpers, FormikValues } from 'formik';
 import { useFormik } from 'formik';
+
 import type * as Yup from 'yup';
 
 import { Button } from 'src/components/atoms/Button';
 import { Container } from 'src/components/atoms/Container';
 import type { InputProps } from 'src/components/atoms/Input';
 import { Input } from 'src/components/atoms/Input';
-
-import styles from './Form.module.css';
 
 interface FormProps<T> {
   initialValues?: T;
@@ -30,40 +28,42 @@ export const Form = <T extends FormikValues>({
   errorMessage,
 }: FormProps<T>) => {
   const formik = useFormik<T>({
-    initialValues: initialValues,
-    validationSchema: validationSchema,
-    onSubmit: onSubmit,
+    initialValues,
+    validationSchema,
+    onSubmit,
   });
 
   return (
-    <form onSubmit={formik.handleSubmit} className={styles.form}>
-      <Container direction="column" alignItems="center">
-        {inputs?.map(input => (
-          <Input
-            key={input.name}
-            {...input}
-            size={'xl'}
-            value={formik.values[input.name] || ''}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            validationState={
-              formik.touched[input.name] &&
-              formik.errors[input.name] &&
-              'invalid'
-            }
-            errorMessage={formik.errors[input.name] as FormikErrors<string>}
-            errorPlacement={'inside'}
-          />
-        ))}
+    <form onSubmit={formik.handleSubmit}>
+      <Container direction="column" alignItems="center" width="100%" gap={4}>
+        <Container direction="column" alignItems="center">
+          {inputs?.map(input => (
+            <Input
+              key={input.name}
+              {...input}
+              size="xl"
+              value={formik.values[input.name] || ''}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              validationState={
+                formik.touched[input.name] &&
+                formik.errors[input.name] &&
+                'invalid'
+              }
+              errorMessage={formik.errors[input.name] as FormikErrors<string>}
+              errorPlacement="inside"
+            />
+          ))}
+        </Container>
+        {errorMessage && (
+          <Text variant="body-short" color="danger">
+            {errorMessage}
+          </Text>
+        )}
+        <Button view="action" type="submit" loading={isSubmitting}>
+          Submit
+        </Button>
       </Container>
-      {errorMessage && (
-        <Text variant={'body-short'} color={'danger'}>
-          {errorMessage}
-        </Text>
-      )}
-      <Button view={'action'} type="submit" loading={isSubmitting}>
-        Submit
-      </Button>
     </form>
   );
 };
