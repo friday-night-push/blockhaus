2c7aad2b8a7fd8a198b57e19d06c475c2218aff5
Anton Zabelin <Libarus@users.noreply.github.com>
2024-08-12 09:27:24 +0300

    Update main.tsx

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -10,9 +10,8 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { App } from './components';
-import { store } from './store';
 
-import { store } from './store/store';
+import { store } from './store';
 
 import './index.css';
 
