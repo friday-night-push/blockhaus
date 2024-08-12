bd1eef2281f639801ce9690740c091795de5709f
Khizri Makhmudov <k.makhmudov@tinkoff.ru>
2024-08-12 10:07:08 +0300

    refactor(FNP-55): correct imports after rebase

--- packages/client/src/utils/get-response-or-throw/get-response-or-throw.ts
+++ packages/client/src/utils/get-response-or-throw/get-response-or-throw.ts
@@ -1,4 +1,6 @@
-export const getResponseOrThrow = async (response: Response): Promise<Response> => {
+export const getResponseOrThrow = async (
+  response: Response
+): Promise<Response> => {
   if (!response.ok) {
     const errorData = await response.json();
     throw new Error(errorData.reason || 'Server Error');
