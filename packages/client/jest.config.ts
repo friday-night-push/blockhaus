00df433bd7a7d59548477b33f10e5f5ebd556304
Kseylo <alekseysarychev29@gmail.com>
2024-08-20 21:12:36 +0700

    feat(FNP-59): added @testing-library/jest-dom, updated jest config

--- packages/client/jest.config.ts
+++ packages/client/jest.config.ts
@@ -1,16 +1,21 @@
 import dotenv from 'dotenv';
+import type { Config } from 'jest';
+
 dotenv.config();
 
-export default {
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^.+\\.css$': 'identity-obj-proxy',
     '^.+\\.svg$': 'jest-svg-transformer',
     'src/(.*)$': '<rootDir>/src/$1',
   },
 };
+
+export default config;
