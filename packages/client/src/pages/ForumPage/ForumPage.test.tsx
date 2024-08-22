495ab18d2c01cdd501471084e22d9ddc094e877c
Kseylo <alekseysarychev29@gmail.com>
2024-08-22 22:18:15 +0700

    fix(FNP-59): forum page added key for each link

---
+++ packages/client/src/pages/ForumPage/ForumPage.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { render, screen, waitFor } from '@testing-library/react';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+
+import { PAGE_ROUTES } from 'src/utils/constants';
+
+import { ForumPage, topicsLoader } from './ForumPage';
+
+test('renders topics and links correctly', async () => {
+  const routes = [
+    {
+      path: PAGE_ROUTES.FORUM,
+      element: <ForumPage />,
+      loader: topicsLoader,
+    },
+  ];
+
+  const router = createMemoryRouter(routes, {
+    initialEntries: [PAGE_ROUTES.FORUM],
+  });
+
+  render(<RouterProvider router={router} />);
+
+  await waitFor(() => {
+    const announcementLink = screen.getByText('Announcement');
+    const complaintsLink = screen.getByText('Complaints');
+
+    expect(announcementLink).toBeInTheDocument();
+    expect(announcementLink.getAttribute('href')).toBe(
+      `${PAGE_ROUTES.FORUM}/1/1`
+    );
+
+    expect(complaintsLink).toBeInTheDocument();
+    expect(complaintsLink.getAttribute('href')).toBe(`${PAGE_ROUTES.FORUM}/2`);
+  });
+});
