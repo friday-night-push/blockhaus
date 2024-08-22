14a71932275eee42f5e491db55ee686b58ab36ea
Kseylo <alekseysarychev29@gmail.com>
2024-08-22 22:41:31 +0700

    feat(FNP-59): protected route tests

---
+++ packages/client/src/hoc/ProtectedRoute/ProtectedRoute.test.tsx
@@ -0,0 +1,47 @@
+import { PAGE_ROUTES } from 'src/utils/constants';
+import { render, screen } from 'src/utils/tests';
+
+import { mockedUseNavigate, mockedUser } from 'src/utils/tests/mocks';
+
+import { ProtectedRoute } from './ProtectedRoute';
+
+describe('ProtectedRoute', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  const testElement = <div>Test</div>;
+
+  it('show loading if user is loading', () => {
+    render(<ProtectedRoute element={testElement} />, {
+      authState: {
+        user: null,
+        userIsLoading: true,
+      },
+    });
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('navigate to sign in if user is not authenticated', () => {
+    render(<ProtectedRoute element={testElement} />, {
+      authState: {
+        user: null,
+        userIsLoading: false,
+      },
+    });
+
+    expect(mockedUseNavigate).toHaveBeenCalledWith(PAGE_ROUTES.SIGN_IN);
+  });
+
+  it('renders the element if user is authenticated', () => {
+    render(<ProtectedRoute element={testElement} />, {
+      authState: {
+        user: mockedUser,
+        userIsLoading: false,
+      },
+    });
+
+    expect(screen.getByText('Test')).toBeInTheDocument();
+  });
+});
