ce659ded1ae5283060c5120419be077caa39eaf4
Kseylo <alekseysarychev29@gmail.com>
2024-08-22 22:29:33 +0700

    fix(FNP-59): removed app test

--- packages/client/src/components/organisms/App/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from '@jest/globals';
-
-// TODO: Find out a reason why tests fail
-describe('mock', () => {
-  test('mock to pass CI/CD', () => {
-    expect(true).toBe(true);
-  });
-});
