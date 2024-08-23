4253ebf45e0785ea58c3a739515673a1fcf085fe
Kseylo <alekseysarychev29@gmail.com>
2024-08-23 11:48:30 +0700

    fix(FNP-59): added whatwg-fetch to polyfill "window.fetch", configured fileTransformer for jest to be able to use static files in tests, basic app.test that tests app rendering

--- packages/client/jest.config.ts
+++ packages/client/jest.config.ts
@@ -20,6 +20,10 @@ const config: Config = {
     '^.+\\.svg$': 'jest-svg-transformer',
     'src/(.*)$': '<rootDir>/src/$1',
   },
+  transform: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/fileTransformer.js',
+  },
 };
 
 export default config;
--- packages/client/jest.setup.ts
+++ packages/client/jest.setup.ts
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import { configure } from '@testing-library/react';
+import 'whatwg-fetch';
 
 configure({ testIdAttribute: 'data-qa' });
 
@@ -7,16 +8,4 @@ window.ResizeObserver = jest.fn().mockImplementation(() => ({
   observe: jest.fn(),
   unobserve: jest.fn(),
   disconnect: jest.fn(),
-}));
-
-if (!globalThis.Response) {
-  // @ts-expect-error response is not defined in jsdom
-  globalThis.Response = jest.fn();
-}
-
-global['Request'] = jest.fn().mockImplementation(() => ({
-  signal: {
-    removeEventListener: jest.fn(),
-    addEventListener: jest.fn(),
-  },
 }));
