2e9002d8e39e88ee8f8da23a54a44d4ef51b39c4
Kseylo <alekseysarychev29@gmail.com>
2024-08-26 20:17:41 +0700

    feat(FNP-59): ssr initial setup

---
+++ packages/client/ssr.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { ThemeProvider } from '@gravity-ui/uikit';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+
+import { App } from './src/components/organisms/App';
+
+export async function render(uri: string) {
+  return {
+    html: renderToString(
+      <StaticRouter location={uri}>
+        <ThemeProvider theme={'light'}>
+          <App />
+        </ThemeProvider>
+      </StaticRouter>
+    ),
+  };
+}
