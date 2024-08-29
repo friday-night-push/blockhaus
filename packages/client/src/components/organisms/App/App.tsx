a054aafa68f7061a4ac048a607c6a01ad62ff8b3
Kseylo <alekseysarychev29@gmail.com>
2024-08-29 13:59:03 +0700

    feat(FNP-59): made router to work with ssr

--- packages/client/src/components/organisms/App/App.tsx
+++ packages/client/src/components/organisms/App/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
+import { ThemeProvider } from '@gravity-ui/uikit';
+
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
+
+import type { createBrowserRouter } from 'react-router-dom';
 import { RouterProvider } from 'react-router-dom';
 
 import { AuthProvider } from 'src/hoc/AuthProvider';
-import { router } from 'src/router';
 
-export const App = () => {
+interface AppProps {
+  router: ReturnType<typeof createBrowserRouter>;
+}
+
+export const App = (props: AppProps) => {
+  const { router } = props;
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={'light'}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </AuthProvider>
   );
 };
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 
-import { ThemeProvider } from '@gravity-ui/uikit';
-
-import '@gravity-ui/uikit/styles/fonts.css';
-import '@gravity-ui/uikit/styles/styles.css';
-
 import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 
+import { createBrowserRouter } from 'react-router-dom';
+
 import { App } from 'src/components/organisms/App';
+import { routes } from 'src/router';
 import { store } from 'src/store';
 
 import './index.css';
 
+const router = createBrowserRouter(routes);
+
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
   <React.StrictMode>
-    <ThemeProvider theme={'light'}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <App router={router} />
+    </Provider>
   </React.StrictMode>
 );
