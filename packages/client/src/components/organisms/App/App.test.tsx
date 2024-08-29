61245818f596a9bb75a99cf1e411cca0bf3e879a
Kseylo <alekseysarychev29@gmail.com>
2024-08-29 22:06:06 +0700

    fix(FNP-59): fixed app test after updates, add --silent option to get rid of warnings, because it's not possible to update @testing/library without touching github actions

--- packages/client/src/components/organisms/App/App.test.tsx
+++ packages/client/src/components/organisms/App/App.test.tsx
@@ -1,10 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { createBrowserRouter } from 'react-router-dom';
+
+import { routes } from 'src/router';
 
 import { App } from './App';
 
 describe('App', () => {
   it('should render', () => {
-    render(<App />);
+    const router = createBrowserRouter(routes);
+    render(<App router={router} />);
     expect(screen.getByText(/ade in 2024 by/i)).toBeInTheDocument();
   });
 });
