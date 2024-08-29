8483f1020e2c9cb81cdebbd62d3646acd99f2da5
Kseylo <alekseysarychev29@gmail.com>
2024-08-29 13:59:03 +0700

    feat(FNP-59): made router to work with ssr

--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,17 +1,68 @@
 import React from 'react';
 
-import { ThemeProvider } from '@gravity-ui/uikit';
+//eslint-disable-next-line
+import type { Request, Response } from 'express';
 import { renderToString } from 'react-dom/server';
 
-import { App } from './src/components/organisms/App';
+import {
+  createStaticRouter,
+  createStaticHandler,
+} from 'react-router-dom/server';
 
-export async function render(uri: string) {
+import { App } from './src/components/organisms/App';
+import { routes } from './src/router';
+
+const handler = createStaticHandler(routes);
+
+export function createFetchRequest(req: Request, res: Response) {
+  const origin = `${req.protocol}://${req.get('host')}`;
+  const url = new URL(req.originalUrl || req.url, origin);
+
+  const controller = new AbortController();
+  res.on('close', () => controller.abort());
+
+  const headers = new Headers();
+
+  for (const [key, values] of Object.entries(req.headers)) {
+    if (values) {
+      if (Array.isArray(values)) {
+        for (const value of values) {
+          headers.append(key, value);
+        }
+      } else {
+        headers.set(key, values);
+      }
+    }
+  }
+
+  const init = {
+    method: req.method,
+    headers,
+    signal: controller.signal,
+    body: undefined,
+  };
+
+  if (req.method !== 'GET' && req.method !== 'HEAD') {
+    init.body = req.body;
+  }
+
+  return new Request(url.href, init);
+}
+
+export async function render(req: Request, res: Response) {
+  const fetchRequest = createFetchRequest(req, res);
+  const context = await handler.query(fetchRequest);
+
+  if (context instanceof Response) {
+    throw context;
+  }
+
+  const router = createStaticRouter(handler.dataRoutes, context);
+
   return {
     html: renderToString(
       <React.StrictMode>
-        <ThemeProvider theme={'light'}>
-          <App />
-        </ThemeProvider>
+        <App router={router} />
       </React.StrictMode>
     ),
   };
