181ac0b02f9c860ef3641096c2ab47611f5b005d
Khizri Makhmudov <kh.makhmudov@me.com>
2024-09-02 17:54:41 +0300

    feat(FNP-60): add user API methods for profile, avatar update, and user search

--- packages/client/src/shared/types/user.ts
+++ packages/client/src/shared/types/user.ts
@@ -1,26 +1,42 @@
-export type TUser = {
-  id: number;
+type TUserCommon = {
   first_name: string;
   second_name: string;
-  display_name: string;
-  avatar: string;
-  email: string;
+  display_name?: string;
   login: string;
+  email: string;
   phone: string;
 };
 
-export type TSignUpRequest = {
-  email: string;
-  first_name: string;
-  second_name: string;
-  login: string;
+type TPassword = {
   password: string;
-  phone: string;
 };
+
+export type TUser = TUserCommon & {
+  id?: number;
+  avatar?: string;
+};
+
+export type TSignUpRequest = TUserCommon & TPassword;
 
 export type TSignInRequest = {
   login: string;
-  password: string;
+} & TPassword;
+
+export type TUserProfileRequest = TUserCommon;
+
+export type TUserPassword = {
+  current_password: string;
+  new_password: string;
+  confirm_password: string;
+};
+
+export type TUserPasswordRequest = {
+  oldPassword: string;
+  newPassword: string;
+};
+
+export type TFindUserRequest = {
+  login: string;
 };
 
 export type TErrorFn = (e: Error) => void;
