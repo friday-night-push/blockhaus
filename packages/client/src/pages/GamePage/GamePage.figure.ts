59ee33262410312c48d1d8b8c0831b6cebf24b9c
Anton Zabelin <azabelin@t1-consulting.ru>
2024-09-10 00:03:25 +0300

    Удаление строк. Фигуру можно поставить только на свободное место

--- packages/client/src/pages/GamePage/GamePage.figure.ts
+++ packages/client/src/pages/GamePage/GamePage.figure.ts
@@ -80,4 +80,85 @@ export class GpFigure {
 
     return figures;
   }
+
+  public static KillRowsAndColumns(field: number[]) {
+    const rows: number[] = [];
+    const cols: number[] = [];
+
+    // Получаем заполненные колонки
+    // столбец
+    for (let i = 0; i < 10; i++) {
+      // строка
+      let fillCol = 0;
+      for (let j = 0; j < 10; j++) {
+        if (field[i * 10 + j] != 0) fillCol++;
+      }
+
+      if (fillCol == 10) {
+        cols.push(i);
+      }
+    }
+
+    // Получаем заполненные строки
+    // строка
+    for (let j = 0; j < 10; j++) {
+      // столбец
+      let fillRow = 0;
+      for (let i = 0; i < 10; i++) {
+        if (field[i * 10 + j] != 0) fillRow++;
+      }
+
+      if (fillRow == 10) {
+        rows.push(j);
+      }
+    }
+
+    if (rows.length > 0) {
+      for (let row = 0; row < 5; row++) {
+        if (rows.indexOf(row) > -1) {
+          // строка есть
+          for (let j = row; j >= 0; j--) {
+            for (let i = 0; i < 10; i++) {
+              field[i * 10 + j] = j == 0 ? 0 : field[i * 10 + j - 1];
+            }
+          }
+        }
+      }
+
+      for (let row = 9; row > 4; row--) {
+        if (rows.indexOf(row) > -1) {
+          // строка есть
+          for (let j = row; j < 10; j++) {
+            for (let i = 0; i < 10; i++) {
+              field[i * 10 + j] = j == 9 ? 0 : field[i * 10 + j + 1];
+            }
+          }
+        }
+      }
+    }
+
+    if (cols.length > 0) {
+      for (let col = 0; col < 5; col++) {
+        if (cols.indexOf(col) > -1) {
+          // строка есть
+          for (let i = col; i >= 0; i--) {
+            for (let j = 0; j < 10; j++) {
+              field[i * 10 + j] = i == 0 ? 0 : field[(i - 1) * 10 + j];
+            }
+          }
+        }
+      }
+
+      for (let col = 9; col > 4; col--) {
+        if (cols.indexOf(col) > -1) {
+          // строка есть
+          for (let i = col; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+              field[i * 10 + j] = i == 9 ? 0 : field[(i + 1) * 10 + j];
+            }
+          }
+        }
+      }
+    }
+  }
 }
