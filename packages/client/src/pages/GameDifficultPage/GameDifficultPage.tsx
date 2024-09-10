3bd891502ce6203315ad5ff62625c6e0147148dc
Ekaterina Kuzina <katya.cuzina2011@yandex.ru>
2024-09-10 21:09:28 +0300

    FNP-79: add slice for gameType

--- packages/client/src/pages/GameDifficultPage/GameDifficultPage.tsx
+++ packages/client/src/pages/GameDifficultPage/GameDifficultPage.tsx
@@ -1,6 +1,9 @@
 import type { FC } from 'react';
+import { useEffect } from 'react';
 
 import { Menu } from '@gravity-ui/uikit';
+
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Container } from 'src/components/atoms/Container';
 import { Logo } from 'src/components/atoms/Logo';
@@ -12,23 +15,30 @@ import { Page } from 'src/components/organisms/Page';
 
 import { PAGE_ROUTES } from 'src/utils/constants';
 
+import type { RootState } from '../../store';
+import { setGameType } from '../../store';
+
 export interface GameProps {
   gameType: number;
 }
 
-const goGame = (gameType: number) => {
-  // сохранить в хранилище gameType
-  // и вызвать PAGE_ROUTES.GAME
-};
-
-export const MENU_ITEMS: MenuItemProps[] = [
-  { label: 'easy', href: PAGE_ROUTES.GAME, onClick: () => goGame(0) },
-  { label: 'medium', href: PAGE_ROUTES.GAME, onClick: () => goGame(1) },
-  { label: 'hard', href: PAGE_ROUTES.GAME, onClick: () => goGame(2) },
-  { label: 'nightmare', href: PAGE_ROUTES.GAME, onClick: () => goGame(3) },
-];
-
 export const GameDifficultPage: FC<GameProps> = ({ gameType }) => {
+  const dispatch = useDispatch();
+  const gameTypeState = useSelector((state: RootState) => state.example.gameType);
+
+  const goGame = (gameType: number) => {
+    // сохранить в хранилище gameType
+    // и вызвать PAGE_ROUTES.GAME
+    dispatch(setGameType(gameType));
+  };
+
+  const MENU_ITEMS: MenuItemProps[] = [
+    { label: 'easy', href: PAGE_ROUTES.GAME, onClick: () => goGame(0) },
+    { label: 'medium', href: PAGE_ROUTES.GAME, onClick: () => goGame(1) },
+    { label: 'hard', href: PAGE_ROUTES.GAME, onClick: () => goGame(2) },
+    { label: 'nightmare', href: PAGE_ROUTES.GAME, onClick: () => goGame(3) },
+  ];
+
   return (
     <Page>
       <Logo isFull size='auto' />
