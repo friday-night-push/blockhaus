d7986e370c43290e4a04700dae33fde57bb8358a
Anton Zabelin <azabelin@t1-consulting.ru>
2024-09-12 16:42:47 +0300

    fix tests

--- packages/client/src/components/organisms/App/App.test.tsx
+++ packages/client/src/components/organisms/App/App.test.tsx
@@ -1,14 +1,23 @@
 import { render, screen } from '@testing-library/react';
+
+import { Provider } from 'react-redux';
+
 import { createBrowserRouter } from 'react-router-dom';
 
 import { routes } from 'src/router';
+import { createStore } from 'src/store';
 
 import { App } from './App';
 
 describe('App', () => {
   it('should render', () => {
+    const store = createStore();
     const router = createBrowserRouter(routes);
-    render(<App router={router} />);
+    render(
+      <Provider store={store}>
+        <App router={router} />
+      </Provider>
+    );
     expect(screen.getByText(/ade in 2024 by/i)).toBeInTheDocument();
   });
 });
--- packages/client/src/pages/GameMenuPage/GameMenuPage.test.tsx
+++ packages/client/src/pages/GameMenuPage/GameMenuPage.test.tsx
@@ -1,11 +1,17 @@
 import userEvent from '@testing-library/user-event';
 
+import { Provider } from 'react-redux';
+
+import { createStore } from 'src/store';
+
 import { PAGE_ROUTES } from 'src/utils/constants';
 import { render, screen } from 'src/utils/tests';
 
 import { mockedUser, mockedUseNavigate } from 'src/utils/tests/mocks';
 
-import { GameMenuPage, MENU_ITEMS } from './GameMenuPage';
+import { GameMenuPage } from './GameMenuPage';
+
+const store = createStore();
 
 describe('GameMenuPage', () => {
   beforeEach(() => {
@@ -13,44 +19,48 @@ describe('GameMenuPage', () => {
   });
 
   it('renders sign in option when user is not logged in', () => {
-    render(<GameMenuPage />, {
-      authState: { user: null, setUser: jest.fn(), userIsLoading: false },
-    });
+    render(
+      <Provider store={store}>
+        <GameMenuPage />
+      </Provider>,
+      {
+        authState: { user: null, setUser: jest.fn(), userIsLoading: false },
+      }
+    );
 
     expect(screen.getByText('sign in')).toBeInTheDocument();
   });
 
   it('renders user component when user is logged in', () => {
-    render(<GameMenuPage />, {
-      authState: {
-        user: mockedUser,
-        userIsLoading: false,
-      },
-    });
+    render(
+      <Provider store={store}>
+        <GameMenuPage />
+      </Provider>,
+      {
+        authState: {
+          user: mockedUser,
+          userIsLoading: false,
+        },
+      }
+    );
     expect(screen.getByText(mockedUser.display_name)).toBeInTheDocument();
   });
 
   it('renders all menu items correctly', () => {
-    render(<GameMenuPage />);
-
-    MENU_ITEMS.forEach(item => {
-      expect(screen.getByText(item.label)).toBeInTheDocument();
-    });
-  });
-
-  it('navigates to menu item that is clicked', async () => {
-    render(<GameMenuPage />);
-
-    const user = userEvent.setup();
-    const menuItem = MENU_ITEMS[0];
-
-    await user.click(screen.getByText(menuItem.label));
-
-    expect(mockedUseNavigate).toHaveBeenCalledWith(menuItem.href);
+    render(
+      <Provider store={store}>
+        <GameMenuPage />
+      </Provider>
+    );
+    expect(screen.getByText('never-ending')).toBeInTheDocument();
   });
 
   it('navigates to sign in page when menu item is clicked', async () => {
-    render(<GameMenuPage />);
+    render(
+      <Provider store={store}>
+        <GameMenuPage />
+      </Provider>
+    );
 
     const user = userEvent.setup();
     await user.click(screen.getByText('sign in'));
