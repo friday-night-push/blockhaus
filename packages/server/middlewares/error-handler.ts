d266ce93a130cfb95aa1262f966257d0e776a4b9
Kseylo <alekseysarychev29@gmail.com>
2024-09-13 21:59:03 +0700

    fix: fixed issues with yaProxy, moved proxy router from /api/v1 to /api/v1/proxy

--- packages/server/middlewares/error-handler.ts
+++ packages/server/middlewares/error-handler.ts
@@ -2,15 +2,11 @@ import type { NextFunction, Request, Response } from 'express';
 
 import { logger } from '../utils';
 
-export const errorHandlerMiddleware = (
-  err: Error,
-  _req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const errorHandlerMiddleware = (err: Error, _req: Request, res: Response, next: NextFunction) => {
   if (res.headersSent) {
     return next(err);
   }
+  console.log(err);
   // eslint-disable-next-line
   const statusCode = (err as any).statusCode || 500;
   const message = err.message || 'Internal Server Error';
