f9fc18e3f780cdbdf4d2271f471cfb9ddc3baba5
Kseylo <alekseysarychev29@gmail.com>
2024-09-23 14:52:19 +0700

    refactor(FNP-80): moved updateProfile and updateAvatar to user-api inside rtk query, changed logic in ProfilePage to use it.

--- packages/client/src/pages/ProfilePage/ProfilePage.tsx
+++ packages/client/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useState } from 'react';
+import { useCallback } from 'react';
 
 import { Avatar, Loader, Text, useFileInput } from '@gravity-ui/uikit';
 
@@ -9,19 +9,20 @@ import { Container } from 'src/components/atoms/Container';
 import { Form } from 'src/components/molecules';
 import { BackButton } from 'src/components/molecules/BackButton';
 import { Page } from 'src/components/organisms/Page';
-import { AuthContext } from 'src/hoc/AuthProvider';
 import { inputs } from 'src/pages/ProfilePage/ProfilePage.constants';
 import UserAPI from 'src/services/api/user-api';
 import type { TUser } from 'src/shared/types/user';
 import { userProfileValidationSchema } from 'src/shared/validation/user';
+import { useGetUserQuery, useUpdateAvatarMutation, useUpdateProfileMutation } from 'src/store/features';
+import { isFetchBaseQueryError } from 'src/utils/api-errors';
 import { PAGE_ROUTES, RESOURCE_URL } from 'src/utils/constants';
-import Helpers from 'src/utils/helpers';
 
 export const userAPI = new UserAPI();
 
 export const ProfilePage = () => {
-  const { user, setUser, userIsLoading } = useContext(AuthContext);
-  const [error, setError] = useState<string | undefined>(undefined);
+  const { data: user } = useGetUserQuery();
+  const [updateAvatar] = useUpdateAvatarMutation();
+  const [updateProfile, { error: updateProfileError }] = useUpdateProfileMutation();
 
   const onUpdate = useCallback((files: File[]) => {
     files[0] && updateUserAvatar(files[0]);
@@ -34,68 +35,57 @@ export const ProfilePage = () => {
 
     formData.append('avatar', avatar);
 
-    await userAPI.updateAvatar(formData, updUserData, errorHandler);
+    await updateAvatar(formData);
   };
 
   const updateUserInfo = async (user: TUser, { setSubmitting }: FormikHelpers<TUser>) => {
     setSubmitting(true);
-
-    await userAPI.updateProfile(user, updUserData, errorHandler).finally(() => {
-      setSubmitting(false);
-    });
+    await updateProfile(user).finally(() => setSubmitting(false));
   };
 
-  const updUserData = (user: TUser) => {
-    if (setUser) {
-      setUser(user);
+  const getErrorMessage = () => {
+    if (isFetchBaseQueryError(updateProfileError)) {
+      return (updateProfileError.data as { reason?: string })?.reason;
     }
   };
 
-  const errorHandler = (err: unknown) => {
-    setError(String(err));
-    Helpers.Log('ERROR', err);
-  };
+  if (user) {
+    const name = user?.display_name || `${user?.first_name} ${user?.second_name}`;
+    return (
+      <Page>
+        <Container direction='column' gap='2' alignItems='center' width='100%'>
+          <input {...controlProps} />
+          <Button style={{ height: '50px', width: '50px' }} pin='circle-circle' view='flat' {...triggerProps}>
+            <Avatar size='xl' imgUrl={user?.avatar ? `${RESOURCE_URL}${user.avatar}` : undefined} text={name} />
+          </Button>
+          <Text variant='display-1'>{name}</Text>
+        </Container>
+
+        <Form
+          initialValues={user}
+          inputs={inputs}
+          validationSchema={userProfileValidationSchema}
+          inputView='clear'
+          onSubmit={updateUserInfo}
+          errorMessage={getErrorMessage()}
+          submitButtonText='Save'
+          showEditButton={true}
+          showResetButton={true}
+        />
+
+        <Container direction='column' width='100%'>
+          <Button isNavigate navigateTo={PAGE_ROUTES.CHANGE_PASSWORD} view='flat-danger' width='max'>
+            Change Password
+          </Button>
+          <BackButton fallbackRoute={PAGE_ROUTES.MENU} />
+        </Container>
+      </Page>
+    );
+  }
 
   return (
     <Page>
-      {userIsLoading ? (
-        <Loader qa='loader' />
-      ) : (
-        <>
-          <Container direction='column' gap='2' alignItems='center' width='100%'>
-            <input {...controlProps} />
-            <Button style={{ height: '50px', width: '50px' }} pin='circle-circle' view='flat' {...triggerProps}>
-              <Avatar
-                size='xl'
-                imgUrl={user?.avatar ? `${RESOURCE_URL}${user.avatar}` : undefined}
-                text={user?.display_name || user?.first_name}
-              />
-            </Button>
-            <Text variant='display-1'>{user ? user.display_name || `${user.first_name} ${user.second_name}` : ''}</Text>
-          </Container>
-
-          {user && (
-            <Form
-              initialValues={user}
-              inputs={inputs}
-              validationSchema={userProfileValidationSchema}
-              inputView='clear'
-              onSubmit={updateUserInfo}
-              errorMessage={error}
-              submitButtonText='Save'
-              showEditButton={true}
-              showResetButton={true}
-            />
-          )}
-
-          <Container direction='column' width='100%'>
-            <Button isNavigate navigateTo={PAGE_ROUTES.CHANGE_PASSWORD} view='flat-danger' width='max'>
-              Change Password
-            </Button>
-            <BackButton fallbackRoute={PAGE_ROUTES.MENU} />
-          </Container>
-        </>
-      )}
+      <Loader qa='loader' size='l' />
     </Page>
   );
 };
